43f09ae021d085420f0f72e9788e4b496f1661ef
Bagwells <timmyayomidebagwells@outlook.com>
2026-03-24 23:39:50 +0100

    upt: submission error fix

--- submission/03.sh
+++ submission/03.sh
@@ -2,8 +2,6 @@
 # Add funds to the address.
 # Return only the Address
 
-bitcoin-cli -regtest loadwallet "btrustwallet"
-
-ADDRESS=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress)
+ADDRESS=$(bitcoin-cli -regtest getnewaddress)
 bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS"
 
