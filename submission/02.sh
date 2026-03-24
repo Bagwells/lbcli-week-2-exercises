20725e7e45a773f7931967e4790804ba7cdb0e21
Bagwells <timmyayomidebagwells@outlook.com>
2026-03-24 17:36:29 +0100

    upt:submission error fix

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a new Bitcoin address, for receiving change.
 
-change_address=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getchangeaddress)
+bitcoin-cli -regtest -rpcwallet="btrustwallet" getchangeaddress
